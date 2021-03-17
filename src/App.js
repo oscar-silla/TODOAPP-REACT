f5c7132adae4767147a643bcd5b8e9e645d81ce4
Oscar Silla <oscarsilla00@gmail.com>
2021-03-17 10:17:18 +0100

    third commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormComponent from './components/FormComponent'
+
+const App = () => {
+    return (
+        <div className='container mt-4'>
+            <FormComponent></FormComponent>
+        </div>
+    )
+}
+
+export default App;
